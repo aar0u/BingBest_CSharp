d6608b2dd1d31f9102ff8387b87ecddbdd4c3fac
aaron <aaron.y.lu@avanade.com>
2018-07-31 11:24:07 +0800

    random get wallpaper

---
+++ Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BingWallpaper
+{
+    public class Logger
+    {
+        private readonly BlockingCollection<string> _bc = new BlockingCollection<string>();
+
+        // Constructor create the thread that wait for work on .GetConsumingEnumerable()
+        public Logger(string file)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                foreach (var message in _bc.GetConsumingEnumerable())
+                    using (var streamWriter = new StreamWriter(file, true))
+                    {
+                        streamWriter.WriteLine($"{DateTime.Now} {message}");
+                    }
+            });
+        }
+
+        ~Logger()
+        {
+            // Free the writing thread
+            _bc.CompleteAdding();
+        }
+
+        public void Info(string msg)
+        {
+            _bc.Add(msg);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
-using System.Text;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 
@@ -10,56 +9,60 @@ namespace BingWallpaper
     internal class Program
     {
         private const string Domain = "https://www.bing.com";
-        private const string RequestUri = Domain + "/HPImageArchive.aspx?format=js&idx=0&n=1";
 
-        private const string UserAgent =
-            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
-
-        private const string SavePath = @".\pic";
-        private static readonly StringBuilder StringBuilder = new StringBuilder();
+        //8 is the max the api returns
+        private const string RequestUrl = Domain + "/HPImageArchive.aspx?format=js&idx=0&n=8";
+        private const string UserAgent = "Mozilla/5.0 (Windows NT 6.1; rv:28.0) Gecko/20100101 Firefox/28.0";
+        private const string SavePath = "pic";
+        private static readonly Random Rnd = new Random();
+        private static readonly Logger Logger = new Logger("log.txt");
 
         private static void Main(string[] args)
         {
-            //Application.Run(new Form1());
-
             var systemTray = new SystemTray("BingWallpaper");
-            systemTray.ShowBalloon("Working on it");
 
+            var index = Rnd.Next(0, 8);
+            systemTray.ShowBalloon($"Set {index} of 8");
+            var file = GetImage(index);
+
+            Logger.Info($"Current: {Wallpaper.GetBackgroud()}");
+            Wallpaper.SetBackgroud(file);
+
+            if (systemTray.TrayIcon != null)
+            {
+                systemTray.TrayIcon.Visible = false;
+                systemTray.TrayIcon.Dispose();
+            }
+
+            Application.Exit();
+        }
+
+        private static string GetImage(int index)
+        {
             using (var httpClient = new HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Add("User-Agent", UserAgent);
 
-                var response = httpClient.GetStringAsync(RequestUri).Result;
-                var imageUrl = Domain + JObject.Parse(response)["images"][0]["url"].Value<string>();
-                var imageResponse = httpClient.GetAsync(imageUrl).Result;
+                var response = httpClient.GetStringAsync(RequestUrl).Result;
+                var image = JObject.Parse(response)["images"][index];
+                var imageUrl = Domain + image["url"].Value<string>();
 
-                var fn = Path.GetFileName(imageUrl);
                 if (!Directory.Exists(SavePath))
                     Directory.CreateDirectory(SavePath);
-                var responseStream = imageResponse.Content.ReadAsStreamAsync().Result;
-                var file = Path.GetFullPath(Path.Combine(SavePath, fn));
-                if (!File.Exists(file))
+
+                var fileName = Path.GetFileName(imageUrl);
+                Logger.Info($"New: {fileName} | {image["copyright"].Value<string>()}");
+
+                var file = Path.GetFullPath(Path.Combine(SavePath, fileName));
+                if (File.Exists(file))
+                    return file;
+                using (var fileStream = File.Create(file))
                 {
-                    using (var fileStream = File.Create(file))
-                    {
-                        responseStream.CopyTo(fileStream);
-                    }
+                    var imageResponse = httpClient.GetStreamAsync(imageUrl).Result;
+                    imageResponse.CopyTo(fileStream);
                 }
 
-                StringBuilder.Append($"{DateTime.Now} Current: {Wallpaper.GetBackgroud()}\r\n");
-                Wallpaper.SetBackgroud(file);
-                StringBuilder.Append($"{DateTime.Now} New: {imageUrl}\r\n");
-
-                File.AppendAllText("log.txt", StringBuilder.ToString());
-                StringBuilder.Clear();
-
-                if (systemTray.TrayIcon != null)
-                {
-                    systemTray.TrayIcon.Visible = false;
-                    systemTray.TrayIcon.Dispose();
-                }
-
-                Application.Exit();
+                return file;
             }
         }
     }
